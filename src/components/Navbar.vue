dca5c9c40d541616e182c1f2e85cc9273ea95a8b
Guilherme Tinen <gfmi0911@hotmail.com>
2023-10-13 22:20:06 -0300

    feat: navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const emits = defineEmits<{
+  (e: "close"): void;
+}>();
+
+const navbarElement = ref(null);
+onClickOutside(navbarElement, () => {
+  emits("close");
+});
+</script>
+
+<template>
+  <div
+    class="flex items-start py-10 px-[50px] fixed inset-0 bg-black/30 backdrop-blur-sm z-50"
+  >
+    <Transition name="slide-anchor-left" :appear="true">
+      <div
+        ref="navbarElement"
+        class="flex flex-col bg-white h-full w-[50%] rounded"
+      ></div>
+    </Transition>
+  </div>
+</template>
