a12331235b8dff3e854db72628023c69ed6a1320
Guilherme Tinen <gfmi0911@hotmail.com>
2023-10-13 22:19:35 -0300

    index page and default layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+const showSearch = ref(false);
+const showNavbar = ref(false);
+
+const handleSearchState = () => {
+  showNavbar.value = false;
+  showSearch.value = !showSearch.value;
+};
+
+const handleNavbarState = () => {
+  showSearch.value = false;
+  showNavbar.value = !showNavbar.value;
+};
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <Header
+      @handleSearchState="handleSearchState"
+      @handleNavbarState="handleNavbarState"
+    />
+    <Transition name="from-opacity">
+      <SearchComponent v-if="showSearch" @close="handleSearchState" />
+    </Transition>
+    <Transition name="from-opacity">
+      <Navbar v-if="showNavbar" @close="handleNavbarState" />
+    </Transition>
+
+    <slot />
+  </div>
+</template>
